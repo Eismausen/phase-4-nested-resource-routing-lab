801af4ec5b278582c2cca073cc75f40c84a87917
David Avery <eismausen@gmail.com>
2021-12-18 20:00:04 -0800

    commit tree 2ccd03333ea1a743b3823f1f53d7406218cc9c6e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
+  def show_items
+    user = User.find(params[:user_id])
+    render json: user.items
+  end
+
+
+  def show_item
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  def create_item
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+
+  get '/users/:user_id/items', to: 'users#show_items'
+  get '/users/:user_id/items/:id', to: 'users#show_item'
+  post '/users/:user_id/items', to: 'users#create_item'
+  
 end
